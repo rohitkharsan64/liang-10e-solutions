b56d2bdb187bf7dd8da64596204f0e21e7dc59ae
Rohit Kharsan <rohitkharsan@outlook.com>
2020-06-17 13:13:18 +0530

    Fix value of pi in exercise 1.8
    More precision required to match answer.

--- chapter01/Exercise01_08.java
+++ chapter01/Exercise01_08.java
@@ -3,7 +3,7 @@
  */
 public class Exercise01_08 {
     public static void main(String[] args) {
-        System.out.println(5.5 * 5.5 * 3.14);
-        System.out.println(2 * 5.5 * 3.14);
+        System.out.println(5.5 * 5.5 * 3.14159);
+        System.out.println(2 * 5.5 * 3.14159);
     }
 }
