5e5821e5462979fefc9689e2610d70c7631e688d
Rohit Kharsan <rohitkharsan@outlook.com>
2020-06-17 13:15:41 +0530

    Fix length of rectangle in exercise 1.9

--- chapter01/Exercise01_09.java
+++ chapter01/Exercise01_09.java
@@ -3,7 +3,7 @@
  */
 public class Exercise01_09 {
     public static void main(String[] args) {
-        System.out.println(4.5 * 7.5);
-        System.out.println(2 * (4.5 + 7.5));
+        System.out.println(4.5 * 7.9);
+        System.out.println(2 * (4.5 + 7.9));
     }
 }
